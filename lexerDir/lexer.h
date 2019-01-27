c412d7d1f1c7c688519b67a59b6918bbb83c75c4
Katie Zucker <katie.r.zucker@gmail.com>
2019-01-27 02:55:01 -0600

    excellent :)))

---
+++ lexerDir/lexer.h
@@ -0,0 +1,43 @@
+// @author Katie Zucker
+
+#include <ctype.h>
+
+typedef enum{
+	OBRACKET,
+	CBRACKET,
+	SEMI,
+	OPAREN,
+	CPAREN,
+	OBRACE,
+	CBRACE,
+	COMMENT,
+	IF,
+	ELSE,
+	RETURN,
+	ID,
+	INTEGER,
+	REAL,
+	STRING,
+	STRUCT,
+	FUNCTION,
+	VARIABLE,
+	BAD_NUMBER,
+	BAD_LEXEME
+} types;
+
+typedef struct{
+	types type;
+	char *id;
+	char *string;
+	int integer;
+	double real;
+	int line;
+} Lexeme;
+
+
+FILE *input;
+int linenum;
+
+extern Lexeme *lex();
+extern void skipWhiteSpace();
+
---
+++ lexerDir/scanner.c
@@ -0,0 +1,78 @@
+// scanner.c
+// @author Katie Zucker
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "lexer.h"
+
+void printLex(Lexeme *l){
+	if (l == NULL)
+		return;
+	if (l->type == OBRACKET)
+		printf("OBRACKET\n");
+	else if (l->type == CBRACKET)
+		printf("CBRACKET\n");
+	else if (l->type == SEMI)
+		printf("SEMI\n");
+	else if (l->type == OPAREN)
+		printf("OPAREN\n");
+	else if (l->type == CPAREN)
+		printf("CPAREN\n");
+	else if (l->type == OBRACE)
+		printf("OBRACE\n");
+	else if (l->type == CBRACE)
+		printf("CBRACE\n");
+	else if (l->type == COMMENT)
+		printf("COMMENT\n");
+	else if (l->type == STRING)
+		printf("STRING \"%s\"\n", l->string);
+	else if (l->type == REAL)
+		printf("REAL %lf\n", l->real);
+	else if (l->type == INTEGER)
+		printf("INTEGER %d\n", l->integer);
+	else if (l->type == IF)
+		printf("IF\n");
+	else if (l->type == ELSE)
+		printf("ELSE\n");
+	else if (l->type == RETURN)
+		printf("RETURN\n");
+	else if (l->type == STRUCT)
+		printf("STRUCT\n");
+	else if (l->type == FUNCTION)
+		printf("FUNCTION\n");
+	else if (l->type == VARIABLE)
+		printf("VARIABLE\n");
+	else if (l->type == ID)
+		printf("ID %s\n", l->id);
+	else if (l->type == BAD_NUMBER){
+		printf("Fatal error: BAD_NUMBER lexeme on line %d\n", l->line);
+		exit(-1);
+	}
+	else if (l->type == BAD_LEXEME){
+		fprintf(stderr, "Fatal error: BAD LEXEME on line %d\n", 
+			l->line);
+		exit(-1);
+	}
+	else{
+		printf("unknown lexeme\n");
+	}
+}
+
+int main(int argc, char **argv){
+	if (argc < 2){
+		fprintf(stderr, "Fatal error: too few args");
+		exit(-1);
+	}
+
+	input = fopen(argv[1], "r");
+
+	while (!feof(input)){
+		skipWhiteSpace();
+		printLex(lex());
+	}
+	//printLex(l);
+
+	fclose(input);
+
+	return 0;
+}
